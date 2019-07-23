a0f6db4287c2a41a186c4037628f1f674a20c0b0
LEVIS <levinsibi@gmail.com>
2019-07-23 20:41:06 +0530

    test change

--- src/jenkins/NewTest.java
+++ src/jenkins/NewTest.java
@@ -12,7 +12,7 @@ public static void test1()
 WebDriver driver=new FirefoxDriver();
 driver.get("https://www.facebook.com/");
 driver.manage().window().maximize();
-driver.findElement(By.xpath("//input[@id='email']")).sendKeys("sachi@b.com");
+driver.findElement(By.xpath("//input[@id='email']")).sendKeys("sach0000@b.com");
 driver.findElement(By.xpath("//input[@id='pass']")).sendKeys("pass");
 System.out.println(driver.getTitle());
 }
