e9bc334331f3c39c2c9a1139aaa5e36fd3c16b06
levinsibi <levinsibi@gmail.com>
2020-08-22 15:13:36 +0530

    test commit

--- src/jenkins/NewTest.java
+++ src/jenkins/NewTest.java
@@ -23,7 +23,7 @@ WebDriver driver=new FirefoxDriver();
 driver.get("https://www.facebook.com/");
 driver.manage().window().maximize();
 driver.findElement(By.xpath("//input[@id='email']")).sendKeys("levi@b.com");
-driver.findElement(By.xpath("//input[@id='pass']")).sendKeys("pass1");
+driver.findElement(By.xpath("//input[@id='pass']")).sendKeys("pass11");
 System.out.println(driver.getTitle());
 }
 
