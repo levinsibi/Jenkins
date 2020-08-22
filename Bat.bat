c03018375bcf3ef1a98f2094dad5d10f0aa6a8d5
levinsibi <levinsibi@gmail.com>
2020-08-22 15:30:33 +0530

    batch file update

--- Bat.bat
+++ Bat.bat
@@ -1,4 +1,4 @@
-set projectLocation=D:\Git2\JenkinsJob
+set projectLocation=D:\WS_Jenkins\Git\Jenkins
 cd %projectLocation%
 set classpath=%projectLocation%\bin;%projectLocation%\lib\*;
 java org.testng.TestNG testng.xml
